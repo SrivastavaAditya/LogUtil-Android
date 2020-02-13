b99f2f56ea7352dbc46d995a9c391400062e523a
Aditya Srivastava <aditya.srivastav@jublfood.com>
2020-02-13 15:16:04 +0530

    library local addition

--- app/src/main/java/com/example/logutilexample/MainActivity.java
+++ app/src/main/java/com/example/logutilexample/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.log_util.LogDebug;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LogDebug.d("Library working");
     }
 }
