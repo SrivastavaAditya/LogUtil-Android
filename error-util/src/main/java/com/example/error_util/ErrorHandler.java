5fe31dcf05a0a0149e3801fbb185706882dec369
Aditya Srivastava <aditya.srivastav@jublfood.com>
2020-02-13 15:35:24 +0530

    error util library added

---
+++ error-util/src/main/java/com/example/error_util/ErrorHandler.java
@@ -0,0 +1,31 @@
+package com.example.error_util;
+
+import android.content.Context;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class ErrorHandler {
+
+    public static final String TAG = "ERROR_HANDLER";
+    private static ErrorHandler mErrorHandler = null;
+    private Context mContext;
+
+    private ErrorHandler(Context context){
+        this.mContext = context;
+    }
+
+    public ErrorHandler getInstance(Context context){
+        if(mErrorHandler == null){
+            mErrorHandler = new ErrorHandler(context);
+        }
+        return mErrorHandler;
+    }
+
+    public void toastError(String message){
+        Toast.makeText(mContext, message, Toast.LENGTH_LONG).show();
+    }
+
+    public void editTextError(EditText editText, String message){
+        editText.setError(message);
+    }
+}
